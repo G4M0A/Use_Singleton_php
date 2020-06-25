a21905ba2b3b3dc9a5b7e366e94405234782a387
G4M0A <64117365+G4M0A@users.noreply.github.com>
2020-06-25 17:55:15 -0500

    Add files via upload

---
+++ singleto.php
@@ -0,0 +1,38 @@
+<?php
+
+class MySingleton //inicia clase
+{
+    private static $instance;
+    private $counter;
+
+    private function __construct()
+    {
+        echo 'Contruyendo objeto.. <br>'.PHP_EOL;
+    }
+
+    public static function getInstance()
+    {
+        if (!self::$instance instanceof self) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public function hazAlgo()
+    {
+        echo 'Escribimos algo por pantalla. <br>'.PHP_EOL;
+        ++$this->counter;
+    }
+    public function getCounter()
+    {
+        return $this->counter;
+    }
+}
+
+MySingleton::getInstance()->hazAlgo();
+MySingleton::getInstance()->hazAlgo();
+MySingleton::getInstance()->hazAlgo();
+
+echo 'Contador = '.mySingleton::getInstance()->getCounter().PHP_EOL;
+?>
